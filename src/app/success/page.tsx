40ce4aa7761db8714f035f52cba9e7531996c197
syed zohaib shah <160512319+zohaib617@users.noreply.github.com>
2025-02-08 22:17:37 +0500

    Add files via upload

---
+++ src/app/success/page.tsx
@@ -0,0 +1,10 @@
+export default function Success() {
+    return (
+      <div className="text-center mt-20">
+        <h1 className="text-3xl font-bold">Payment Successful!</h1>
+        <p className='text-white font-bold text-xl' >Thank you for your purchase.</p>
+        <a href="/" className="text-blue-500 underline">Go back to Home</a>
+      </div>
+    );
+  }
+  
