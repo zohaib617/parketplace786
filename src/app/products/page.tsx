1c163dc5addbddd17142ed3a5bd33168c5e81423
syed zohaib shah <160512319+zohaib617@users.noreply.github.com>
2025-02-11 10:02:27 +0500

    Update page.tsx
    gird

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -107,7 +107,7 @@ export default function FoodList({ selectedCategories }: FoodListProps) {
 
       <div className="w-[332px] h-[46px]"></div>
 
-      <div className="grid grid-cols-3 gap-4 w-[800px] ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full lg:w-[800px]">
         {products.map((product) => (
           <div key={product._id} className="">
             <Link href={`/products/${product._id}`}>
